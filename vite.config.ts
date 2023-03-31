9f13a690f5ba2233ba5d6c7660b51dee307706e5
Sandeep Shaw <sandeep10shaw@gmail.com>
2023-03-31 23:26:49 +0530

    Page Redirect Baesname

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   define: {
     'process.env.SUPABASE_URL': `"${process.env.SUPABASE_URL}"`,
